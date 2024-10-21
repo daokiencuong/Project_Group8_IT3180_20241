cb15b2cf54d8e729254541fc0fdad0284f307e5c
tuan6100 <tuananhho378@gmail.com>
2024-10-21 23:44:34 +0700

    update project

--- src/main/java/com/web/project/repository/ProjectRepo.java
+++
@@ -1,8 +0,0 @@
-// package com.web.project.repository;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import com.web.project.model.entity.Project;
-
-// public interface ProjectRepo extends JpaRepository<Project, String>{
-//     // list interface
-// }
